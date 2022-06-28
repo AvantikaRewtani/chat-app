afe28f64dca89fddfbdf1ed175aa90023c9b64ff
AvantikaRewtani <rewtaniav@rknec.edu>
2022-06-28 22:40:23 +0530

    frontend api created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import {Route} from "react-router-dom"
 import './App.css';
+import ChatPage from './pages/ChatPage';
+import Homepage from './pages/Homepage';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      <Route path ="/" component ={Homepage}/>    
+      <Route path ="/chats" component ={ChatPage}/>    
+</div>
   );
 }
-
 export default App;
+
