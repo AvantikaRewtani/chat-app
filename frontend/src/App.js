46158da7b93a3afd90fbebc6f56c91f7030b09d5
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2022-07-02 10:09:42 +0530

    Axios added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+// import { Button } from "@chakra-ui/button";
+import { Route } from "react-router-dom";
 import "./App.css";
+import ChatPage from "./pages/ChatPage";
+import Homepage from "./pages/Homepage";
 
 function App() {
-	return <div className="App">Hello</div>;
+	return (
+		<div className="App">
+			{/* <Button colorScheme="blue"> Button</Button> */}
+			<Route path="/" component={Homepage} exact />
+			<Route path="/chats" component={ChatPage} />
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
